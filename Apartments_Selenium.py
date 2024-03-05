fb7b22213b612fd18c8fae3d58c8ecb520a92875
ArgentCode <blackponder@gmail.com>
2024-03-05 16:04:49 -0600

    Trying to add the regex

--- Apartments_Selenium.py
+++ Apartments_Selenium.py
@@ -5,8 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-import csv
 import re
+import pandas as pd
 
 college_file_list = ['ames-ia', 'college-station-tx']
 # college_file_list = []
@@ -34,15 +34,42 @@ for city in college_file_list:
     driver.get(url)
 
     html = driver.page_source
+    prices = PriceExtraction(html)
     time.sleep(2)
-    filename = city + "-html.txt"
-    with open(filename, 'w') as file:
-        file.write(html.encode('utf-8').decode('ascii', 'ignore'))
-    file.close()
+    # filename = city + "-html.txt"
+    # with open(filename, 'w') as file:
+    #     file.write(html.encode('utf-8').decode('ascii', 'ignore'))
+    # file.close()
     ## Insert some gabe code here! 
     i= i+1
     time.sleep(3)
-    if i > 2:
+    if i > 1:
         break
 
 driver.close()
+
+def PriceExtraction(html):
+
+    # Remove tabs and newlines
+    pricing = re.sub("[\t|\n]+", "", html)
+
+    # # Extract all matches
+    matches = re.findall(r'aria-label="[\\w\\s[:punct:]]*>[^<]*<p class="property-pricing">[^<]*</p>', pricing)
+
+    # # Filter out None values and convert to DataFrame
+    pricing_df = pd.DataFrame([x for x in matches if x is not None], columns=['Text'])
+
+    # # Separate AptName and Price
+    # pricing_df[['AptName', 'Price']] = pricing_df['Text'].str.extract(r'aria-label="([^"]*)">[^<]*<p class="property-pricing">([^<]*)</p>')
+
+    # # Clean AptName and Price columns
+    # pricing_df['AptName'] = pricing_df['AptName'].str.replace('aria-label="', '').str.replace(',.*$', '')
+    # pricing_df['Price'] = pricing_df['Price'].str.replace('class="property-pricing">\\$', '').str.replace('</p>', '')
+
+    # # Separate Price into Price1 and Price2
+    # pricing_df[['Price1', 'Price2']] = pricing_df['Price'].str.split(' - ', expand=True)
+
+    # # Drop the original Text and Price columns
+    # pricing_df.drop(columns=['Text', 'Price'], inplace=True)
+    print(pricing_df)
+    return pricing_df
