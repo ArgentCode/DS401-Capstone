38e12b9f2ed29f31d8f246b689cff6bf37d18ff0
ArgentCode <blackponder@gmail.com>
2024-03-07 15:57:19 -0600

    Refined for the recording

--- Apartments_Selenium.py
+++ Apartments_Selenium.py
@@ -8,17 +8,17 @@ import time
 import re
 import pandas as pd
 
-college_file_list = ['ames-ia', 'college-station-tx']
-# college_file_list = []
+college_file_list = ['ames-ia', 'college-station-tx', 'minneapolis-mn']
+# college_file_list = []/
     
     # Use the pattern to match and filter characters
     
 
-with open('Collegetowns2.csv', 'r') as file:
-    # Create a CSV reader object using DictReader
-    for line in file:
-        city = line[:-1].replace(',', '-').replace(' ', '-')
-        college_file_list.append(city)
+# with open('Collegetowns2.csv', 'r') as file:
+#     # Create a CSV reader object using DictReader
+#     for line in file:
+#         city = line[:-1].replace(',', '-').replace(' ', '-')
+#         college_file_list.append(city)
 
 options=Options()
 options.add_experimental_option("detach", True)
@@ -34,7 +34,6 @@ for city in college_file_list:
     driver.get(url)
 
     html = driver.page_source
-    prices = PriceExtraction(html)
     time.sleep(2)
     # filename = city + "-html.txt"
     # with open(filename, 'w') as file:
@@ -42,34 +41,7 @@ for city in college_file_list:
     # file.close()
     ## Insert some gabe code here! 
     i= i+1
-    time.sleep(3)
-    if i > 1:
+    if i > 3:
         break
 
 driver.close()
-
-def PriceExtraction(html):
-
-    # Remove tabs and newlines
-    pricing = re.sub("[\t|\n]+", "", html)
-
-    # # Extract all matches
-    matches = re.findall(r'aria-label="[\\w\\s[:punct:]]*>[^<]*<p class="property-pricing">[^<]*</p>', pricing)
-
-    # # Filter out None values and convert to DataFrame
-    pricing_df = pd.DataFrame([x for x in matches if x is not None], columns=['Text'])
-
-    # # Separate AptName and Price
-    # pricing_df[['AptName', 'Price']] = pricing_df['Text'].str.extract(r'aria-label="([^"]*)">[^<]*<p class="property-pricing">([^<]*)</p>')
-
-    # # Clean AptName and Price columns
-    # pricing_df['AptName'] = pricing_df['AptName'].str.replace('aria-label="', '').str.replace(',.*$', '')
-    # pricing_df['Price'] = pricing_df['Price'].str.replace('class="property-pricing">\\$', '').str.replace('</p>', '')
-
-    # # Separate Price into Price1 and Price2
-    # pricing_df[['Price1', 'Price2']] = pricing_df['Price'].str.split(' - ', expand=True)
-
-    # # Drop the original Text and Price columns
-    # pricing_df.drop(columns=['Text', 'Price'], inplace=True)
-    print(pricing_df)
-    return pricing_df
